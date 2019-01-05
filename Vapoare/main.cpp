8208110a3ccf47b401a05674a3b340391dd42b9f
Elisei Timofte <timofte@unfuddle.com>
2019-01-05 13:44:48 +0200

    more work done

--- Vapoare/main.cpp
+++ Vapoare/main.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 /*! GLUT display callback function */
 void display(void);
-/*! GLUT window reshape callback function */
 void reshape(int, int);
-void onMouseClick(int button, int state, int x, int y);
+void onMouseClick(int, int, int, int);
+
+void renderGrid(int, int, bool);
+void renderUserBattleShips();
+void renderStrikesOnBattleFields(bool);
+int getLeftOffset(bool);
 
 int WIDTH = 1280;
 int HEIGHT = 760;
@@ -15,33 +19,200 @@ int ENEMY_LEFT_OFFSET = 100;
 int USER_LEFT_OFFSET = 600;
 int BF_SIZE = 400;
 int STEP_SIZE = BF_SIZE/10;
-int USER_BATTLE_FIELD[10][10]=
+int USER_BATTLE_FIELD[10][10];
+int ENEMY_BATTLE_FIELD[10][10];
+int ROUND = 1;
+
+int USER_SHIPS[4][4][3]=
 {
-  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {{1,2,0},{1,3,0}, {-1,-1,-1}, {-1,-1,-1}},
+  {{5,2,0},{6,3,0}, {-1,-1,-1}, {-1,-1,-1}},
+  {{6,5,0},{6,6,0}, {6,7,0}, {-1,-1,-1}},
+  {{1,4,0},{2,5,0}, {3,6,0}, {4,7,0}},
 };
 
-int ENEMY_BATTLE_FIELD[10][10]=
+void renderBattleField(int originX, int originY, bool isEnemyBattleField)
 {
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 2, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-};
+  cout<<"Render Battle field "<<isEnemyBattleField<<"\n";
+  renderGrid(originX, originY, isEnemyBattleField);
+  
+  int strikeNo;
+  int leftOffset = getLeftOffset(isEnemyBattleField);
+  
+  if (!isEnemyBattleField) {
+    renderUserBattleShips();
+  }
+  
+  renderStrikesOnBattleFields(isEnemyBattleField);
+}
+
+void onMouseClick(int button, int state, int x, int y)
+{
+  bool toogleRender = 0;
+  bool clickedOnEnemy = x < (ENEMY_LEFT_OFFSET+BF_SIZE) && x > ENEMY_LEFT_OFFSET && y > TOP_OFFSET && y < (TOP_OFFSET + BF_SIZE);
+  bool clickedOnUser = x < (USER_LEFT_OFFSET+BF_SIZE) && x > USER_LEFT_OFFSET && y > TOP_OFFSET && y < (TOP_OFFSET + BF_SIZE);
+  int leftOffset = clickedOnEnemy ? ENEMY_LEFT_OFFSET : USER_LEFT_OFFSET;
+  
+  // left click   && Button_up
+  if (button == 0 && state == 1) {
+    int xIndex = (x - leftOffset)/STEP_SIZE;
+    int yIndex = (y - TOP_OFFSET)/STEP_SIZE;
+    
+    if (clickedOnEnemy){
+      ENEMY_BATTLE_FIELD[yIndex][xIndex] = 2;
+      toogleRender = 1;
+    }
+    if (clickedOnUser){
+      USER_BATTLE_FIELD[yIndex][xIndex] = 2;
+      toogleRender = 1;
+    }
+    ROUND++;
+  }
+  
+  if (toogleRender) {
+    glutPostRedisplay();
+  }
+  
+}
+
+void renderGrid(int originX, int originY, bool isEnemyBattleField)
+{
+  if (isEnemyBattleField) {
+    glColor3f(1, 0, 0);
+  } else {
+    glColor3f(1, 1, 1);
+  }
+  
+  glBegin(GL_LINE_LOOP);
+  glVertex2f(originX,           originY          );
+  glVertex2f(originX + BF_SIZE, originY          );
+  glVertex2f(originX + BF_SIZE, originY + BF_SIZE);
+  glVertex2f(originX,           originY + BF_SIZE);
+  glEnd();
+  
+  glBegin(GL_LINES);
+  for (int i=0; i<BF_SIZE; i+=STEP_SIZE) {
+    glVertex2f(originX          , originY + i       );
+    glVertex2f(originX + BF_SIZE, originY + i       );
+    glVertex2f(originX + i      , originY           );
+    glVertex2f(originX + i      , originY + BF_SIZE );
+  }
+  glEnd();
+}
+
+int getLeftOffset(bool isEnemyBattleField)
+{
+  if (isEnemyBattleField) {
+    return ENEMY_LEFT_OFFSET;
+  } else {
+    return USER_LEFT_OFFSET;
+  }
+}
+
+void renderUserBattleShips()
+{
+  int strikeNo;
+  for (int xi=0; xi<4; xi++) {
+    for (int yi=0; yi<4; yi++) {
+      int x = USER_SHIPS[xi][yi][0];
+      int y = USER_SHIPS[xi][yi][1];
+      strikeNo = USER_SHIPS[xi][yi][2];
+      
+      if (x == -1) {continue;}
+      
+      switch (xi) {
+        case 0:
+          glColor3f(0.0497, 0.5779, 0.71); break;
+        case 1:
+          glColor3f(0.0497, 0.5779, 0.71); break;
+        case 2:
+          glColor3f(0.1602, 0.5251, 0.89); break;
+        case 3:
+          glColor3f(0.1152, 0.2701, 0.96); break;
+      }
+      
+      int realX = x * STEP_SIZE + USER_LEFT_OFFSET;
+      int realY = y * STEP_SIZE + TOP_OFFSET;
+      
+      glBegin(GL_POLYGON);
+      glVertex2f(realX + 2            , realY + 2);
+      glVertex2f(realX + STEP_SIZE - 3 , realY + 2);
+      glVertex2f(realX + STEP_SIZE - 3 , realY + STEP_SIZE - 2);
+      glVertex2f(realX + 2             , realY + STEP_SIZE - 2);
+      glEnd();
+      
+    }
+  }
+}
+
+void renderStrikesOnBattleFields(bool isEnemyBF)
+{
+  int strikeNo;
+  int leftOffset = getLeftOffset(isEnemyBF);
+  
+  for (int xi=0; xi<BF_SIZE; xi+=STEP_SIZE) {
+    for (int yi=0; yi<BF_SIZE; yi+=STEP_SIZE) {
+      int yIndex = yi/STEP_SIZE;
+      int xIndex = xi/STEP_SIZE;
+      
+      if (isEnemyBF) {
+        strikeNo = ENEMY_BATTLE_FIELD[yIndex][xIndex];
+      } else {
+        strikeNo = USER_BATTLE_FIELD[yi/STEP_SIZE][xi/STEP_SIZE];
+      }
+      const double green[3] = {0, 1, 0.4667};
+      if (strikeNo) {
+        
+        glColor3f(1,1,1); break;
+        int realX = xi + leftOffset;
+        int realY = yi + TOP_OFFSET;
+        
+        glBegin(GL_POLYGON);
+          glVertex2f(realX + 10             , realY + 10);
+          glVertex2f(realX + STEP_SIZE - 10 , realY + 10);
+          glVertex2f(realX + STEP_SIZE - 10 , realY + STEP_SIZE - 10);
+          glVertex2f(realX + 10             , realY + STEP_SIZE - 10);
+        glEnd();
+      }
+    }
+  }
+}
+
+void display()
+{
+  /* clear the color buffer (resets everything to black) */
+  glClear(GL_COLOR_BUFFER_BIT);
+  
+  renderBattleField(ENEMY_LEFT_OFFSET, TOP_OFFSET, 1);
+  renderBattleField(USER_LEFT_OFFSET, TOP_OFFSET, 0);
+  
+  /* swap the back and front buffers so we can see what we just drew */
+  glutSwapBuffers();
+}
+
+/*! glut reshape callback function.  GLUT calls this function whenever
+ the window is resized, including the first time it is created.
+ You can use variables to keep track the current window size.
+ */
+void reshape(int width, int height)
+{
+  /* tell OpenGL we want to display in a recangle that is the
+   same size as the window
+   */
+  glViewport(0,0,width,height);
+  
+  /* switch to the projection matrix */
+  glMatrixMode(GL_PROJECTION);
+  
+  /* clear the projection matrix */
+  glLoadIdentity();
+  
+  /* set the camera view, orthographic projection in 2D */
+  gluOrtho2D(0,width,height,0);
+  
+  /* switch back to the model view matrix */
+  glMatrixMode(GL_MODELVIEW);
+}
 
 int main(int argc, char** argv)
 {
@@ -76,7 +247,7 @@ int main(int argc, char** argv)
   glutReshapeFunc(reshape);
   
   /* set the default background color to black */
-//  glClearColor(0,0,0,1);
+  //  glClearColor(0,0,0,1);
   
   /* enter the main event loop so that GLUT can process
    all of the window event messages
@@ -84,126 +255,4 @@ int main(int argc, char** argv)
   glutMainLoop();
   
   return 0;
-}
-
-void renderBattleField(int originX, int originY, bool enemyBattleField)
-{
-  
-  glBegin(GL_LINE_LOOP);
-    glVertex2f(originX,           originY);
-    glVertex2f(BF_SIZE + originX, originY);
-    glVertex2f(BF_SIZE + originX, BF_SIZE+originY);
-    glVertex2f(originX,           BF_SIZE + originY);
-  glEnd();
-  
-  glBegin(GL_LINES);
-    for (int i=0; i<BF_SIZE; i+=STEP_SIZE) {
-      glVertex2f(originX,           i+originY         );
-      glVertex2f(BF_SIZE + originX, i+originY         );
-      glVertex2f(i+originX,         originY           );
-      glVertex2f(i+originX,         BF_SIZE + originY );
-    }
-  glEnd();
-  
-  int cellCode;
-  int leftOffset;
-  
-  for (int xi=0; xi<BF_SIZE; xi+=STEP_SIZE) {
-    for (int yi=0; yi<BF_SIZE; yi+=STEP_SIZE) {
-      
-      if (enemyBattleField) {
-        cellCode = ENEMY_BATTLE_FIELD[yi/STEP_SIZE][xi/STEP_SIZE];
-        
-        // do not render enemy ships positions
-        if (cellCode == 1) {
-          continue;
-        }
-        
-        leftOffset = ENEMY_LEFT_OFFSET;
-      } else {
-        cellCode = USER_BATTLE_FIELD[yi/STEP_SIZE][xi/STEP_SIZE];
-        leftOffset = USER_LEFT_OFFSET;
-      }
-      
-      if (cellCode != 0){
-        switch (cellCode) {
-          case 1:
-            glColor3f(0.5, 0.5, 0); break;
-          case 2:
-            glColor3f(1, 0, 0); break;
-        }
-        
-        int x = xi + leftOffset;
-        int y = yi + TOP_OFFSET;
-        
-        glBegin(GL_POLYGON);
-          glVertex2f(x + 10             , y + 10);
-          glVertex2f(x + STEP_SIZE - 10 , y + 10);
-          glVertex2f(x + STEP_SIZE - 10 , y + STEP_SIZE - 10);
-          glVertex2f(x + 10             , y + STEP_SIZE - 10);
-        glEnd();
-      }
-      
-    }
-  }
-}
-
-void onMouseClick(int button, int state, int x, int y)
-{
-  bool clickedOnEnemy = x < USER_LEFT_OFFSET;
-  int leftOffset = clickedOnEnemy ? ENEMY_LEFT_OFFSET : USER_LEFT_OFFSET;
-  
-  if (button == 0 && state == 1) {
-    int xIndex = (x - leftOffset)/STEP_SIZE;
-    int yIndex = (y - TOP_OFFSET)/STEP_SIZE;
-    
-    if (clickedOnEnemy){
-      ENEMY_BATTLE_FIELD[yIndex][xIndex] = 2;
-    } else {
-      USER_BATTLE_FIELD[yIndex][xIndex] = 2;
-    }
-    
-  }
-  
-  
-  glutPostRedisplay();
-}
-
-void display()
-{
-  /* clear the color buffer (resets everything to black) */
-  glClear(GL_COLOR_BUFFER_BIT);
-  
-  glColor3f(1, 0, 0);
-  renderBattleField(ENEMY_LEFT_OFFSET, TOP_OFFSET, 1);
-  
-  glColor3f(1, 1, 1);
-  renderBattleField(USER_LEFT_OFFSET, TOP_OFFSET, 0);
-  
-  /* swap the back and front buffers so we can see what we just drew */
-  glutSwapBuffers();
-}
-
-/*! glut reshape callback function.  GLUT calls this function whenever
- the window is resized, including the first time it is created.
- You can use variables to keep track the current window size.
- */
-void reshape(int width, int height)
-{
-  /* tell OpenGL we want to display in a recangle that is the 
-   same size as the window
-   */
-  glViewport(0,0,width,height);
-  
-  /* switch to the projection matrix */
-  glMatrixMode(GL_PROJECTION);
-  
-  /* clear the projection matrix */
-  glLoadIdentity();
-  
-  /* set the camera view, orthographic projection in 2D */
-  gluOrtho2D(0,width,height,0);
-  
-  /* switch back to the model view matrix */
-  glMatrixMode(GL_MODELVIEW);
 }
